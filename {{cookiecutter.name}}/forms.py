2039ebe95d7f222f79a702d2bfd9101a8f7c8062
Zeheng Li <imzehengl@gmail.com>
2021-11-09 00:11:18 -0700

    refresh deps

--- {{cookiecutter.name}}/forms.py
+++ {{cookiecutter.name}}/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextField
+from wtforms import SubmitField, StringField
 from wtforms.validators import DataRequired
 
 
 class SomeForm(FlaskForm):
-    input1 = TextField("Input 1", validators=[DataRequired()])
-    input2 = TextField("Input 2", validators=[DataRequired()])
+    input1 = StringField("Input 1", validators=[DataRequired()])
+    input2 = StringField("Input 2", validators=[DataRequired()])
     submit = SubmitField("Submit")
